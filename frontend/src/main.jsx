45bfe924ebd1ad1e771a4fa62dad549de8fcdb53
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-11-08 12:34:24 +0000

    Update main.jsx with MantineProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import '@mantine/core/styles.css';
+import { MantineProvider, createTheme } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
