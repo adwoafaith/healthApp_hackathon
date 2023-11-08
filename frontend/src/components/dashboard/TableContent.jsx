2a1432533c5bd9499753a68bb701f0e76f1c6565
waglarry <emmanuelacquaah2468@gmail.com>
2023-11-08 23:35:38 +0000

    Update TableContent component

--- frontend/src/components/dashboard/TableContent.jsx
+++ frontend/src/components/dashboard/TableContent.jsx
@@ -1,11 +1,11 @@
 import { Table } from '@mantine/core';
+import { Pagination } from '@mantine/core';
 
 const elements = [
   { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
   { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
   { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
   { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
-  { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
   { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
   { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
 ];
@@ -23,19 +23,26 @@ const TableContent = () => {
   ));
 
   return (
-    <Table style={{width: '90%', margin: '0 auto', textAlign: 'center'}} stickyHeader stickyHeaderOffset={60}>
-      <Table.Thead>
-        <Table.Tr>
-          <Table.Th style={{textAlign: 'center'}}>REF ID</Table.Th>
-          <Table.Th style={{textAlign: 'center'}}>FULL NAME</Table.Th>
-          <Table.Th style={{textAlign: 'center'}}>GENDER</Table.Th>
-          <Table.Th style={{textAlign: 'center'}}>CLASS</Table.Th>
-          <Table.Th style={{textAlign: 'center'}}>SUBJECT</Table.Th>
-          <Table.Th style={{textAlign: 'center'}}>ATTENDANCE</Table.Th>
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>{rows}</Table.Tbody>
-    </Table>
+    <div style={{ backgound: '#ffffff', filter: 'drop-shadow(0px 10px 60px rgba(226, 236, 249, 0.50))' }}>
+      <Table style={{ textAlign: 'center' }} stickyHeader verticalSpacing="md" stickyHeaderOffset={60}>
+          <Table.Tr>
+            <Table.Th style={{textAlign: 'center'}}>REF ID</Table.Th>
+            <Table.Th style={{textAlign: 'center'}}>FULL NAME</Table.Th>
+            <Table.Th style={{textAlign: 'center'}}>GENDER</Table.Th>
+            <Table.Th style={{textAlign: 'center'}}>CLASS</Table.Th>
+            <Table.Th style={{textAlign: 'center'}}>SUBJECT</Table.Th>
+            <Table.Th style={{textAlign: 'center'}}>ATTENDANCE</Table.Th>
+          </Table.Tr>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+      <div style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        marginTop: '1rem'
+      }}>
+        <Pagination total={10} color="yellow" size="sm" withEdges />
+      </div>
+    </div>
   );
 }
 
