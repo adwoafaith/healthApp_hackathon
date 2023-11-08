57e365d7585884164fae3e22d087ab3ef8dabb51
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-11-08 16:13:36 +0000

    Update Login with data input function

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from '../css-modules/Login.module.css'
 import Brand from '../components/Brand';
 import Kids from '../assets/images/login-image.svg'
@@ -7,8 +7,35 @@ import BackIcon from '../components/BackIcon';
 import { FaRegUser } from 'react-icons/fa';
 import { FiMail } from 'react-icons/fi';
 import { AiOutlineLock } from 'react-icons/ai';
+import { Select, TextInput, rem } from '@mantine/core';
+import classes from '../css-modules/components/MantineInput.module.css';
+import { PasswordInput } from '@mantine/core';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const LoginPage = () => {
+
+  const [ formData, setFormData ] = useState({})
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted');
+    console.log(formData);
+    
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const userIcon = <FaRegUser style={{ width: rem(12), height: rem(12) }} />;
+  const mailIcon = <FiMail style={{ width: rem(12), height: rem(12) }} />;
+  const lockIcon = <AiOutlineLock style={{ width: rem(12), height: rem(12) }} />;
+
   return (
     <section className={Styles.section}>
       <div className={Styles.imageContainer}>
@@ -25,32 +52,47 @@ const LoginPage = () => {
           <h1 className={Styles.title}>Sign In</h1>
           <p>Enter your credentials to access your account</p>
 
-          <form action="#" className={Styles.formBox}>
+          <form onSubmit={handleFormSubmit} className={Styles.formBox}>
             <div className={Styles.inputField}>
-              <span className={Styles.icon}>
-                <FaRegUser />
-              </span>
-              <select name="" id="">
-                <option value="1">Teacher</option>
-                <option value="2">Parent</option>
-              </select>
+              <Select
+                size='md'
+                radius="md"
+                leftSectionPointerEvents="none"
+                leftSection={userIcon}
+                comboboxProps={{ withinPortal: true }}
+                data={['Parent', 'Teacher']}
+                placeholder="Role"
+                classNames={classes}
+                // onChange={handleInputChange}
+              />
             </div>
             <div className={Styles.inputField}>
-              <span className={Styles.icon}>
-                <FiMail />
-              </span>
-              <input type="text" placeholder='Email' />
+              <TextInput
+                size='md'
+                radius="md"
+                leftSectionPointerEvents="none"
+                leftSection={mailIcon}
+                placeholder="Email"
+                name='email'
+                onChange={handleInputChange}
+              />
             </div>
             <div className={Styles.inputField}>
-              <span className={Styles.icon}>
-                <AiOutlineLock />
-              </span>
-              <input type="password" placeholder='Password' />
+              <PasswordInput
+                size='md'
+                radius="md"
+                leftSectionPointerEvents="none"
+                leftSection={lockIcon}
+                placeholder="Password"
+                name='password'
+                onChange={handleInputChange}
+              />
             </div>
 
-            <p>
+            <div className={Styles.forgotPasswordRow}>
+              <FormControlLabel className={Styles.checkbox} control={<Checkbox defaultChecked />} label="Remeber me?" />
               <Link className={Styles.forgotPassword} to={'/'}>Forgot your password?</Link>
-            </p>
+            </div>
 
             <button className={Styles.button}>Sign In</button>
           </form>
