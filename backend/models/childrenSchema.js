33abc7a32ec331f726675e23bec030ec34d4a942
adwoafaith <adwoafaith49@gmail.com>
2023-11-09 16:36:21 +0000

    integrate

--- backend/models/childrenSchema.js
+++ backend/models/childrenSchema.js
@@ -2,21 +2,30 @@ const mongoose = require('mongoose')
 const express = require('express')
 const Schema = mongoose.Schema;
 
-const ChildSchema = new Schema ({
-    firstName:{
+const ChildSchema = new Schema({
+    firstName: {
         type: String,
     },
-    lastName:{
+    lastName: {
         type: String,
     },
-    gender:{
+    gender: {
         type: String
     },
-    class:{
-        type:String
+    class: {
+        type: String
+    },
+    attendance: {
+        type: Boolean
+    },
+    courseProgress: {
+        type: Object
+    },
+    skill:{
+        type:Object
     }
-    
+
 })
 
-const Child = mongoose.model("Child",ChildSchema)
+const Child = mongoose.model("Child", ChildSchema)
 module.exports = Child;
