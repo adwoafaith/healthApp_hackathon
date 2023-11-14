b3b8eca6f5cd4c43bee29542e968589c7ab3a8c1
Faith Ayeh Mensah <faith.mensah@amalitech.com>
2023-11-14 17:02:59 +0100

    test 2

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const path = require('path');
 const app = express()
 const cors = require('cors')
 const connect = require('./db')
-require('dotenv').config()
+require('dotenv').config({path:".env"})
 const handleError = require('./error/handleError')
 const notFound = require('./middleware/notfound')
 const router = require('./routers')
 const port = process.env.PORT || 5000
 
+console.log(port)
 //middleware
 app.use(cors())
 app.use(express.json())
